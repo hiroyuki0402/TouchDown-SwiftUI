542aa22bb99a4d393feb052a6d1d0f827af8f03d
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2023-10-29 16:07:49 +0900

    準備

---
+++ TopuchDown_swiftui/View/Home/HomeView.swift
@@ -0,0 +1,22 @@
+//
+//  HomeView.swift
+//  TopuchDown_swiftui
+//
+//  Created by SHIRAISHI HIROYUKI on 2023/10/29.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+
+    // MARK: - プロパティー
+
+    // MARK: - ボディー
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }//: MARK - ボディ
+}
+
+#Preview {
+    HomeView()
+}
